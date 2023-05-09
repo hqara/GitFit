be5ac26179546cfe3465ae9a5df549cd85b0248a
sarahcol21 <sarah.colantoni@gmail.com>
2023-05-09 16:30:24 -0400

    sarahc 1 commit

--- GitFit/MainForm.cs
+++ GitFit/MainForm.cs
@@ -18,7 +18,7 @@ namespace GitFit
             InitializeComponent();
         }
         
-        
+        //sarah
         private void showPasswordCheckBox_CheckedChanged(object sender, EventArgs e)
         {
             if (showPasswordCheckBox.Checked)
