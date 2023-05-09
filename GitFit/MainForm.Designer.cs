61b0a3d3c5c7a9e394ab4a743b2f8be4033cab16
2176328 <2176328@D23914-JZRZ0Q2>
2023-05-09 13:14:25 -0400

    Add project files.

---
+++ GitFit/MainForm.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace GitFit
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.usernameTxt = new System.Windows.Forms.TextBox();
+            this.passwordTxt = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.Label();
+            this.confirmPasswordTxt = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.registerBtn = new System.Windows.Forms.Button();
+            this.showPasswordCheckBox = new System.Windows.Forms.CheckBox();
+            this.backToLogin = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.LightSlateGray;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(-3, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(395, 541);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(47, 329);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(297, 116);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lifestyle Management \r\nSystem";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(47, 63);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(297, 280);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(452, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 42);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Sign Up";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(454, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(114, 53);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Username";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // usernameTxt
+            // 
+            this.usernameTxt.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.usernameTxt.ForeColor = System.Drawing.Color.Black;
+            this.usernameTxt.Location = new System.Drawing.Point(458, 143);
+            this.usernameTxt.Name = "usernameTxt";
+            this.usernameTxt.Size = new System.Drawing.Size(322, 28);
+            this.usernameTxt.TabIndex = 3;
+            // 
+            // passwordTxt
+            // 
+            this.passwordTxt.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.passwordTxt.ForeColor = System.Drawing.Color.Black;
+            this.passwordTxt.Location = new System.Drawing.Point(458, 227);
+            this.passwordTxt.Name = "passwordTxt";
+            this.passwordTxt.Size = new System.Drawing.Size(322, 28);
+            this.passwordTxt.TabIndex = 5;
+            this.passwordTxt.UseSystemPasswordChar = true;
+            // 
+            // password
+            // 
+            this.password.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.password.Location = new System.Drawing.Point(454, 180);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(114, 53);
+            this.password.TabIndex = 4;
+            this.password.Text = "Password";
+            this.password.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // confirmPasswordTxt
+            // 
+            this.confirmPasswordTxt.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.confirmPasswordTxt.ForeColor = System.Drawing.Color.Black;
+            this.confirmPasswordTxt.Location = new System.Drawing.Point(458, 318);
+            this.confirmPasswordTxt.Name = "confirmPasswordTxt";
+            this.confirmPasswordTxt.Size = new System.Drawing.Size(322, 28);
+            this.confirmPasswordTxt.TabIndex = 7;
+            this.confirmPasswordTxt.UseSystemPasswordChar = true;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(454, 271);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(189, 53);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Confirm Password";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // registerBtn
+            // 
+            this.registerBtn.BackColor = System.Drawing.Color.LightSlateGray;
+            this.registerBtn.FlatAppearance.BorderColor = System.Drawing.Color.LightSlateGray;
+            this.registerBtn.FlatAppearance.BorderSize = 5;
+            this.registerBtn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.LightSlateGray;
+            this.registerBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.registerBtn.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.registerBtn.ForeColor = System.Drawing.Color.White;
+            this.registerBtn.Location = new System.Drawing.Point(557, 449);
+            this.registerBtn.Name = "registerBtn";
+            this.registerBtn.Size = new System.Drawing.Size(123, 48);
+            this.registerBtn.TabIndex = 8;
+            this.registerBtn.Text = "Register";
+            this.registerBtn.UseVisualStyleBackColor = false;
+            // 
+            // showPasswordCheckBox
+            // 
+            this.showPasswordCheckBox.AutoSize = true;
+            this.showPasswordCheckBox.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.showPasswordCheckBox.Location = new System.Drawing.Point(458, 371);
+            this.showPasswordCheckBox.Name = "showPasswordCheckBox";
+            this.showPasswordCheckBox.Size = new System.Drawing.Size(130, 21);
+            this.showPasswordCheckBox.TabIndex = 9;
+            this.showPasswordCheckBox.Text = "Show Password";
+            this.showPasswordCheckBox.UseVisualStyleBackColor = true;
+            this.showPasswordCheckBox.CheckedChanged += new System.EventHandler(this.showPasswordCheckBox_CheckedChanged);
+            // 
+            // backToLogin
+            // 
+            this.backToLogin.AutoSize = true;
+            this.backToLogin.Font = new System.Drawing.Font("Century Gothic", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.backToLogin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.backToLogin.Location = new System.Drawing.Point(483, 410);
+            this.backToLogin.Name = "backToLogin";
+            this.backToLogin.Size = new System.Drawing.Size(267, 20);
+            this.backToLogin.TabIndex = 10;
+            this.backToLogin.Text = "Already Have an Account? LOG IN";
+            this.backToLogin.Click += new System.EventHandler(this.backToLogin_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(854, 537);
+            this.Controls.Add(this.backToLogin);
+            this.Controls.Add(this.showPasswordCheckBox);
+            this.Controls.Add(this.registerBtn);
+            this.Controls.Add(this.confirmPasswordTxt);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.passwordTxt);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.usernameTxt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Century Gothic", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Git Fit Application";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox usernameTxt;
+        private System.Windows.Forms.TextBox passwordTxt;
+        private System.Windows.Forms.Label password;
+        private System.Windows.Forms.TextBox confirmPasswordTxt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button registerBtn;
+        private System.Windows.Forms.CheckBox showPasswordCheckBox;
+        private System.Windows.Forms.Label backToLogin;
+    }
+}
+
---
+++ GitFit/MainForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace GitFit
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void showPasswordCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (showPasswordCheckBox.Checked)
+            {
+                passwordTxt.UseSystemPasswordChar = false;
+                confirmPasswordTxt.UseSystemPasswordChar = false;
+            }
+            else {
+                //
+                passwordTxt.UseSystemPasswordChar = true;
+                confirmPasswordTxt.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void backToLogin_Click(object sender, EventArgs e)
+        {
+            LoginForm login = new LoginForm();
+            login.Show();
+            Visible = false;
+        }
+    }
+}
