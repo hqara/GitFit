84c72317b40217b4168fa0bd60553aae7dc2a608
hqara <98411846+hqara@users.noreply.github.com>
2023-05-12 18:50:33 -0400

    Delete dbo.User.data.sql

--- GitFit/dbo.User.data.sql
+++
@@ -1,1 +0,0 @@
-﻿INSERT INTO [dbo].[User] ([username], [password], [fname], [lname], [dob], [email], [phone], [gender], [height], [weight]) VALUES (N'hqara', N'hibba123', N'Hibba', N'Qaraman', N'2000-05-11', N'hqara@gmail.com', N'5142323232', N'f', CAST(165.00 AS Decimal(5, 2)), CAST(150.00 AS Decimal(5, 2)))
