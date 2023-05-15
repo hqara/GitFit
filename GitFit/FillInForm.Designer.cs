4d41e2e6bb1603dccdbbfef6e7535fe199866ea9
sarahcol21 <sarah.colantoni@gmail.com>
2023-05-15 11:08:00 -0400

    fix form

--- GitFit/FillInForm.Designer.cs
+++ GitFit/FillInForm.Designer.cs
@@ -64,9 +64,10 @@
             // 
             this.firstNameLabel.AutoSize = true;
             this.firstNameLabel.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.firstNameLabel.Location = new System.Drawing.Point(68, 90);
+            this.firstNameLabel.Location = new System.Drawing.Point(51, 73);
+            this.firstNameLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.firstNameLabel.Name = "firstNameLabel";
-            this.firstNameLabel.Size = new System.Drawing.Size(122, 23);
+            this.firstNameLabel.Size = new System.Drawing.Size(99, 21);
             this.firstNameLabel.TabIndex = 0;
             this.firstNameLabel.Text = "First Name: ";
             // 
@@ -74,9 +75,10 @@
             // 
             this.lastNameLabel.AutoSize = true;
             this.lastNameLabel.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lastNameLabel.Location = new System.Drawing.Point(68, 140);
+            this.lastNameLabel.Location = new System.Drawing.Point(51, 114);
+            this.lastNameLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lastNameLabel.Name = "lastNameLabel";
-            this.lastNameLabel.Size = new System.Drawing.Size(126, 23);
+            this.lastNameLabel.Size = new System.Drawing.Size(101, 21);
             this.lastNameLabel.TabIndex = 1;
             this.lastNameLabel.Text = "Last Name: ";
             // 
@@ -84,9 +86,10 @@
             // 
             this.dateOfBirthLabel.AutoSize = true;
             this.dateOfBirthLabel.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dateOfBirthLabel.Location = new System.Drawing.Point(68, 194);
+            this.dateOfBirthLabel.Location = new System.Drawing.Point(51, 158);
+            this.dateOfBirthLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.dateOfBirthLabel.Name = "dateOfBirthLabel";
-            this.dateOfBirthLabel.Size = new System.Drawing.Size(140, 23);
+            this.dateOfBirthLabel.Size = new System.Drawing.Size(115, 21);
             this.dateOfBirthLabel.TabIndex = 2;
             this.dateOfBirthLabel.Text = "Date of Birth: ";
             // 
@@ -94,9 +97,10 @@
             // 
             this.emailLabel.AutoSize = true;
             this.emailLabel.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.emailLabel.Location = new System.Drawing.Point(68, 250);
+            this.emailLabel.Location = new System.Drawing.Point(51, 203);
+            this.emailLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.emailLabel.Name = "emailLabel";
-            this.emailLabel.Size = new System.Drawing.Size(73, 23);
+            this.emailLabel.Size = new System.Drawing.Size(59, 21);
             this.emailLabel.TabIndex = 4;
             this.emailLabel.Text = "Email: ";
             // 
@@ -104,9 +108,10 @@
             // 
             this.phoneLabel.AutoSize = true;
             this.phoneLabel.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.phoneLabel.Location = new System.Drawing.Point(68, 299);
+            this.phoneLabel.Location = new System.Drawing.Point(51, 243);
+            this.phoneLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.phoneLabel.Name = "phoneLabel";
-            this.phoneLabel.Size = new System.Drawing.Size(168, 23);
+            this.phoneLabel.Size = new System.Drawing.Size(134, 21);
             this.phoneLabel.TabIndex = 5;
             this.phoneLabel.Text = "Phone Number: ";
             // 
@@ -114,9 +119,10 @@
             // 
             this.genderLabel.AutoSize = true;
             this.genderLabel.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.genderLabel.Location = new System.Drawing.Point(68, 354);
+            this.genderLabel.Location = new System.Drawing.Point(51, 288);
+            this.genderLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.genderLabel.Name = "genderLabel";
-            this.genderLabel.Size = new System.Drawing.Size(96, 23);
+            this.genderLabel.Size = new System.Drawing.Size(78, 21);
             this.genderLabel.TabIndex = 6;
             this.genderLabel.Text = "Gender: ";
             // 
@@ -124,9 +130,10 @@
             // 
             this.heightLabel.AutoSize = true;
             this.heightLabel.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.heightLabel.Location = new System.Drawing.Point(68, 405);
+            this.heightLabel.Location = new System.Drawing.Point(51, 329);
+            this.heightLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.heightLabel.Name = "heightLabel";
-            this.heightLabel.Size = new System.Drawing.Size(158, 23);
+            this.heightLabel.Size = new System.Drawing.Size(131, 21);
             this.heightLabel.TabIndex = 7;
             this.heightLabel.Text = "Height (inches):";
             // 
@@ -134,9 +141,10 @@
             // 
             this.weightLabel.AutoSize = true;
             this.weightLabel.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.weightLabel.Location = new System.Drawing.Point(68, 459);
+            this.weightLabel.Location = new System.Drawing.Point(51, 373);
+            this.weightLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.weightLabel.Name = "weightLabel";
-            this.weightLabel.Size = new System.Drawing.Size(119, 23);
+            this.weightLabel.Size = new System.Drawing.Size(100, 21);
             this.weightLabel.TabIndex = 8;
             this.weightLabel.Text = "Weight (lb):";
             // 
@@ -144,9 +152,10 @@
             // 
             this.createAccountLabel.AutoSize = true;
             this.createAccountLabel.Font = new System.Drawing.Font("Century Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.createAccountLabel.Location = new System.Drawing.Point(271, 23);
+            this.createAccountLabel.Location = new System.Drawing.Point(203, 19);
+            this.createAccountLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.createAccountLabel.Name = "createAccountLabel";
-            this.createAccountLabel.Size = new System.Drawing.Size(251, 37);
+            this.createAccountLabel.Size = new System.Drawing.Size(206, 30);
             this.createAccountLabel.TabIndex = 9;
             this.createAccountLabel.Text = "Create Account";
             // 
@@ -156,10 +165,10 @@
             this.submitBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.submitBtn.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.submitBtn.ForeColor = System.Drawing.Color.White;
-            this.submitBtn.Location = new System.Drawing.Point(581, 526);
-            this.submitBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.submitBtn.Location = new System.Drawing.Point(436, 427);
+            this.submitBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.submitBtn.Name = "submitBtn";
-            this.submitBtn.Size = new System.Drawing.Size(144, 64);
+            this.submitBtn.Size = new System.Drawing.Size(108, 52);
             this.submitBtn.TabIndex = 10;
             this.submitBtn.Text = "Submit";
             this.submitBtn.UseVisualStyleBackColor = false;
@@ -168,57 +177,57 @@
             // firstNameTextBox
             // 
             this.firstNameTextBox.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.firstNameTextBox.Location = new System.Drawing.Point(240, 87);
-            this.firstNameTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.firstNameTextBox.Location = new System.Drawing.Point(180, 71);
+            this.firstNameTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.firstNameTextBox.Name = "firstNameTextBox";
-            this.firstNameTextBox.Size = new System.Drawing.Size(485, 32);
+            this.firstNameTextBox.Size = new System.Drawing.Size(365, 27);
             this.firstNameTextBox.TabIndex = 11;
             // 
             // lastNameTextBox
             // 
             this.lastNameTextBox.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lastNameTextBox.Location = new System.Drawing.Point(240, 137);
-            this.lastNameTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.lastNameTextBox.Location = new System.Drawing.Point(180, 111);
+            this.lastNameTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.lastNameTextBox.Name = "lastNameTextBox";
-            this.lastNameTextBox.Size = new System.Drawing.Size(485, 32);
+            this.lastNameTextBox.Size = new System.Drawing.Size(365, 27);
             this.lastNameTextBox.TabIndex = 12;
             // 
             // dateOfBirthTimePicker
             // 
             this.dateOfBirthTimePicker.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dateOfBirthTimePicker.Location = new System.Drawing.Point(240, 188);
-            this.dateOfBirthTimePicker.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dateOfBirthTimePicker.Location = new System.Drawing.Point(180, 153);
+            this.dateOfBirthTimePicker.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dateOfBirthTimePicker.Name = "dateOfBirthTimePicker";
-            this.dateOfBirthTimePicker.Size = new System.Drawing.Size(279, 32);
+            this.dateOfBirthTimePicker.Size = new System.Drawing.Size(364, 27);
             this.dateOfBirthTimePicker.TabIndex = 13;
             // 
             // emailTextBox
             // 
             this.emailTextBox.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.emailTextBox.Location = new System.Drawing.Point(240, 247);
-            this.emailTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.emailTextBox.Location = new System.Drawing.Point(180, 201);
+            this.emailTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.emailTextBox.Name = "emailTextBox";
-            this.emailTextBox.Size = new System.Drawing.Size(485, 32);
+            this.emailTextBox.Size = new System.Drawing.Size(365, 27);
             this.emailTextBox.TabIndex = 14;
             // 
             // phoneTextBox
             // 
             this.phoneTextBox.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.phoneTextBox.Location = new System.Drawing.Point(240, 295);
-            this.phoneTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.phoneTextBox.Location = new System.Drawing.Point(180, 240);
+            this.phoneTextBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.phoneTextBox.MaxLength = 10;
             this.phoneTextBox.Name = "phoneTextBox";
-            this.phoneTextBox.Size = new System.Drawing.Size(341, 32);
+            this.phoneTextBox.Size = new System.Drawing.Size(257, 27);
             this.phoneTextBox.TabIndex = 15;
             // 
             // maleRadioButton
             // 
             this.maleRadioButton.AutoSize = true;
             this.maleRadioButton.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.maleRadioButton.Location = new System.Drawing.Point(240, 352);
-            this.maleRadioButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.maleRadioButton.Location = new System.Drawing.Point(180, 286);
+            this.maleRadioButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.maleRadioButton.Name = "maleRadioButton";
-            this.maleRadioButton.Size = new System.Drawing.Size(80, 27);
+            this.maleRadioButton.Size = new System.Drawing.Size(67, 25);
             this.maleRadioButton.TabIndex = 16;
             this.maleRadioButton.TabStop = true;
             this.maleRadioButton.Text = "Male";
@@ -228,10 +237,10 @@
             // 
             this.femaleRadioButton.AutoSize = true;
             this.femaleRadioButton.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.femaleRadioButton.Location = new System.Drawing.Point(349, 352);
-            this.femaleRadioButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.femaleRadioButton.Location = new System.Drawing.Point(262, 286);
+            this.femaleRadioButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.femaleRadioButton.Name = "femaleRadioButton";
-            this.femaleRadioButton.Size = new System.Drawing.Size(105, 27);
+            this.femaleRadioButton.Size = new System.Drawing.Size(85, 25);
             this.femaleRadioButton.TabIndex = 17;
             this.femaleRadioButton.TabStop = true;
             this.femaleRadioButton.Text = "Female";
@@ -240,30 +249,30 @@
             // heightNumericUpDown
             // 
             this.heightNumericUpDown.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.heightNumericUpDown.Location = new System.Drawing.Point(240, 402);
-            this.heightNumericUpDown.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.heightNumericUpDown.Location = new System.Drawing.Point(180, 327);
+            this.heightNumericUpDown.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.heightNumericUpDown.Maximum = new decimal(new int[] {
             300,
             0,
             0,
             0});
             this.heightNumericUpDown.Name = "heightNumericUpDown";
-            this.heightNumericUpDown.Size = new System.Drawing.Size(95, 32);
+            this.heightNumericUpDown.Size = new System.Drawing.Size(71, 27);
             this.heightNumericUpDown.TabIndex = 18;
             this.heightNumericUpDown.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // weightNumericUpDown
             // 
             this.weightNumericUpDown.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.weightNumericUpDown.Location = new System.Drawing.Point(240, 457);
-            this.weightNumericUpDown.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.weightNumericUpDown.Location = new System.Drawing.Point(180, 371);
+            this.weightNumericUpDown.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.weightNumericUpDown.Maximum = new decimal(new int[] {
             300,
             0,
             0,
             0});
             this.weightNumericUpDown.Name = "weightNumericUpDown";
-            this.weightNumericUpDown.Size = new System.Drawing.Size(95, 32);
+            this.weightNumericUpDown.Size = new System.Drawing.Size(71, 27);
             this.weightNumericUpDown.TabIndex = 19;
             this.weightNumericUpDown.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
@@ -273,10 +282,10 @@
             this.returnFillBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.returnFillBtn.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.returnFillBtn.ForeColor = System.Drawing.Color.White;
-            this.returnFillBtn.Location = new System.Drawing.Point(72, 526);
-            this.returnFillBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.returnFillBtn.Location = new System.Drawing.Point(54, 427);
+            this.returnFillBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.returnFillBtn.Name = "returnFillBtn";
-            this.returnFillBtn.Size = new System.Drawing.Size(144, 64);
+            this.returnFillBtn.Size = new System.Drawing.Size(108, 52);
             this.returnFillBtn.TabIndex = 20;
             this.returnFillBtn.Text = "Return";
             this.returnFillBtn.UseVisualStyleBackColor = false;
@@ -299,15 +308,16 @@
             // tableAdapterManager
             // 
             this.tableAdapterManager.BackupDataSetBeforeUpdate = false;
+            this.tableAdapterManager.TableTableAdapter = null;
             this.tableAdapterManager.UpdateOrder = GitFit.UserDataSetTableAdapters.TableAdapterManager.UpdateOrderOption.InsertUpdateDelete;
             this.tableAdapterManager.UserTableAdapter = this.userTableAdapter;
             // 
             // FillInForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.AliceBlue;
-            this.ClientSize = new System.Drawing.Size(802, 610);
+            this.ClientSize = new System.Drawing.Size(602, 496);
             this.Controls.Add(this.returnFillBtn);
             this.Controls.Add(this.weightNumericUpDown);
             this.Controls.Add(this.heightNumericUpDown);
@@ -329,7 +339,7 @@
             this.Controls.Add(this.lastNameLabel);
             this.Controls.Add(this.firstNameLabel);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FillInForm";
             this.Text = "Git Fit Application";
             this.Load += new System.EventHandler(this.FillInForm_Load);
